ce928eaeb2d6f9cdde51403d0d8333131352382b
Dominic Carvalho <domz.carvalho@gmail.com>
2020-12-09 17:36:26 +0200

    PHP email update

--- send_mail.php
+++ send_mail.php
@@ -3,9 +3,9 @@
 This first bit sets the email address that you want the form to be submitted to.
 You will need to change this value to a valid email address that you can access.
 */
-$webmaster_email = "bookings@blect.co.za";
+$webmaster_email = "chantel@blect.co.za";
 $from = "info@blect.co.za";
-$email_to = "bookings@blect.co.za";
+$email_to = "chantel@blect.co.za";
 /*
 This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
